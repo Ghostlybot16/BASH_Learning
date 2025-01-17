deb18728e4a590b2dcf795917ffeae1e897653a3
Kramptj KC <kramptj@gmail.com>
2025-01-17 18:22:30 -0500

    Edited zeroToDevForLoop.sh to fix the loop
    The loop didn't work previously but now it does due to the
    addition of the new logic. It uses 2 variables to output the final
    message. The first variable stores the output of the for loop int
    with a string message and the second variables uses the output of
    the first along with the word count command to find the first
    variables word count and then the final echo statement outputs
    the two variables together in the format
    "Number <loopCount> : <wordCountOfEntireString>"
    example: "Number 0 : 8"

--- basicLearning/zeroToDevForLoop.sh
+++ basicLearning/zeroToDevForLoop.sh
@@ -2,11 +2,24 @@
 
 # Testing for loop from the book 
 
-for b in {0..3}; do
-	echo "Number $b"
-	echo -n "Number $b" | wc -c
+# for loop outputs values from 0 to 3
+for b in {0..3}; do                          
+	
+	# FIRST stores each output of the for loop integers with the string Number
+	# ex: Number 0, Number 1, Number 2
+	FIRST="Number $b"
+
+	# SECOND uses the string value of FIRST and utilizes the pipe command to find the stings word count as well
+	# The echo command uses "-n" to ignore new lines that the wc command picks up
+	SECOND=$(echo -n "$FIRST" | wc -c)
+
+	echo "$FIRST : $SECOND" # Final output message
 done
 
 for i in {1..5}; do
-	echo "Iteration $i"
+	ONE="Iteration $i"
+	TWO=$(echo -n "$ONE" | wc -c)
+
+	echo "$ONE : $TWO"
+
 done
